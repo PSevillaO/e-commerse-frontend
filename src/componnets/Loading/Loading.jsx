31c062ef9a4006ca30bd95462eda9b09f9dc03d2
Pablo Sevilla <sevillaolivieri@gmail.com>
2024-03-06 22:35:59 -0300

    Loading nuevo en Login y home

--- src/componnets/Loading/Loading.jsx
+++ src/componnets/Loading/Loading.jsx
@@ -1,11 +1,13 @@
-import { Spinner } from "reactstrap";
+// import { Spinner } from "reactstrap";
 // import 'bootstrap/dist/css/bootstrap.min.css'
+import loading from '../../../src/assets/image/Loading.gif'
 
 
 export default function Loading() {
     return (
         <>
-            <Spinner color="danger" />
+            {/* <Spinner color="danger" /> */}
+            <img src={loading} alt="Loading..." />
         </>
 
     )
